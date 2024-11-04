6d0400ce3c0f2055fe4f2484bd3ae9f463a7918c
Khasanbek <khasantuit@gmail.com>
2024-11-04 15:34:32 +0500

    qwertyui

--- api/views.py
+++ api/views.py
@@ -1,18 +1,14 @@
 from rest_framework.generics import *
 from api.models import *
 from .serializers import *
-from rest_framework import permissions
 
 # Test Api
 class TestApiView(ListAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Test.objects.all()
     serializer_class = Testserializer
 class TestApiCreate(ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Test.objects.all()
     serializer_class = Testserializer
 class TestApiUpdate(RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Test.objects.all()
     serializer_class = Testserializer
